976172d020116d7bf90d3f12eae3980ef73dd285
yossi <yrotenstreich@gmail.com>
2020-12-10 14:33:25 -0500

    wrote update method and constructor

--- src/com/jrutz/ElectionReporter.java
+++ src/com/jrutz/ElectionReporter.java
@@ -1,23 +1,36 @@
 package com.jrutz;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Observable;
+import java.util.Observer;
 
-abstract class ElectionReporter {
+abstract class ElectionReporter implements Observer {
 
     IElectionReportingStrategy PVReportStrategy;
     IElectionReportingStrategy ECReportStrategy;
+    ArrayList<State> electionResults;
     SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yy hh:mm:ss a");
 
-    String reportPopularVote() {
-        return PVReportStrategy.getReport();
+    public ElectionReporter(Observable observable) {
+        observable.addObserver(this);
     }
 
-    String reportElectoralCollege() {
-        return ECReportStrategy.getReport();
+    public void update(Observable observable, Object o) {
+        if (observable instanceof ElectionData) {
+            ElectionData electionData = (ElectionData) observable;
+            electionResults = electionData.getStateResults();
+        }
     }
 
-    void DisplayLegalMessage() {
+    public String reportElection() {
+        DisplayLegalMessage();
+        return PVReportStrategy.getReport()
+                + ECReportStrategy.getReport();
+    }
+
+    public void DisplayLegalMessage() {
         System.out.println("\n" + formatter.format(new Date()));
         System.out.println("All reports are purely observational and not legally binding in any way.\n");
     }
